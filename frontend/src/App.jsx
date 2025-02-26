33216dedb27b37ec243da3f818964d0a0c79b767
Ojasvsakhi <ojasvsakhi@gmail.com>
2025-02-26 21:14:55 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';  // Add this import
 import OtpFlow from './components/OtpFlow';
 import Dashboard from './components/DashBoard';
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<OtpFlow />} />
+        <Route path="/" element={<Login />} />  {/* Changed this line */}
+        <Route path="/verify-otp" element={<OtpFlow />} />  {/* Added this route */}
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
