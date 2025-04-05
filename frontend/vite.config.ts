3a85e8a70570fbbcbcb67a7277124d69fb541f78
Ojasvsakhi <ojasvsakhi@gmail.com>
2025-04-05 23:28:04 +0530

    Name change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       manifest: {
         name: 'Crypto Knight',
-        short_name: 'CryKnight',
+        short_name: 'CryptoKnight',
         description: 'Secure Password Manager',
         theme_color: '#111827',
         background_color: '#111827',
